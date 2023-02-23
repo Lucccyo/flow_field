6ab387687178467f3469d41aeb334c7218c425cb
Lucccyo <cha.git@mailo.fr>
2023-02-23 16:59:55 +0100

    improve linzer

--- bin/main.ml
+++ bin/main.ml
@@ -25,33 +25,33 @@ let hor_strip t y len rgb =
     done
   done
 
-let _tarte_linzer t x len palette =
+let tarte_linzer t x len palette =
 (* ajouter le fait de decouper jusqu'en bas *)
   let x = ref x in
   let trou = !x in
   let i_color = ref 0 in
-  while !x + len < 200 do
-    ver_strip t !x len palette.(!i_color);
-    (* hor_strip t !x len palette.(!i_color); *)
-    (* Format.printf "icolor = %d@." (!i_color+1); *)
-    hor_strip t !x len palette.(if !i_color + 1 < Array.length palette then !i_color + 1 else 0);
-    x := !x + trou + len;
-    i_color := (!i_color + 1) mod Array.length palette
-  done
+  try
+    while true do
+      ver_strip t !x len palette.(!i_color);
+      hor_strip t !x len palette.(if !i_color + 1 < Array.length palette then !i_color + 1 else 0);
+      x := !x + trou + len;
+      i_color := (!i_color + 1) mod Array.length palette
+    done
+  with Failure _ -> ()
 
-let rec build_palette arr r g b: Color.rgb array =
+let rec build_palette size arr r g b: Color.rgb array =
   let open Random in
-  if Array.length arr = 4 then arr else
+  if Array.length arr = size then arr else
   let red   = r + (let rand = 14 + (int 50) in if r + rand > 255 then 0 else rand) in
   let green = g + (let rand = 14 + (int 50) in if g + rand > 255 then 0 else rand) in
   let blue  = b + (let rand = 14 + (int 50) in if b + rand > 255 then 0 else rand) in
-  build_palette (Array.append arr [|{r; g; b}|]) red green blue
+  build_palette size (Array.append arr [|{r; g; b}|]) red green blue
 
-let build_palette () =
+let build_palette size =
   let open Random in
-  build_palette [||] (int 256) (int 256) (int 256)
+  build_palette size [||] (int 256) (int 256) (int 256)
 
-let palette_preview t palette =
+let _palette_preview t palette =
   let _dim_x, dim_y = Images.size (Rgb24 t) in
   let len = Array.length palette in
   let strip_size = dim_y / len in
@@ -64,7 +64,7 @@ let () =
   Random.self_init ();
   let rgb24 = Rgb24.create 200 200 in
   try
-    (* tarte_linzer rgb24 17 6 (build_palette ()); *)
-    palette_preview rgb24 (build_palette ());
+    tarte_linzer rgb24 8 5 (build_palette 7);
+    (* palette_preview rgb24 (build_palette 7); *)
     build rgb24
   with Failure e -> Format.printf "ERROR: %s@." e
