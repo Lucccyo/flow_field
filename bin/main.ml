15b6de4c5d4d187ace4912baf06e4b79167e7ef4
Lucccyo <cha.git@mailo.fr>
2023-02-23 11:11:55 +0100

    use Rgb24. set red pixel to 'red'

--- bin/main.ml
+++ bin/main.ml
@@ -1,14 +1,13 @@
+let build t =
+  let filename = "img.png" in
+  try
+    Png.save filename [Images.Save_Quality 1] (Rgb24 t);
+    if Array.length Sys.argv - 1 = 1 then
+      let store = "musée/" ^ Sys.argv.(1) ^ ".png" in
+      Png.save store [Images.Save_Quality 1] (Rgb24 t)
+  with Failure m -> Format.printf "Error %s@." m
+
 let () =
-  let indexed = Index8.create_with 200 170 [] {max = 0; map =
-    [|
-      {r = 255; g = 51 ; b = 51 };
-      {r = 255; g = 255; b = 0  };
-      {r = 255; g = 128; b = 0  };
-      {r = 0  ; g = 204; b = 204}
-      |]} 40 (Bytes.of_string "Meow") in
-  let filename = "meow.png" in
-  let n, m = Images.size (Index8 indexed) in
-  Format.printf "image size : %dx%d\n" n m;
-  Images.save filename (Some Png) [] (Index8 indexed);
-  (* So if img is of type Rgb24.t, then (Rgb24 img) has type Images.t. *)
-  (* let n,m = Images.size img in *)
+  let rgb24 = Rgb24.create 200 170 in
+  Rgb24.set rgb24 0 0 {r = 255; g = 0; b = 0};
+  build rgb24
