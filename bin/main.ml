cf239ffa923fcbb2e362944188385469fe1ddb96
Lucccyo <cha.git@mailo.fr>
2023-02-25 13:50:29 +0100

    debug linz and hor/ver strips

--- bin/main.ml
+++ bin/main.ml
@@ -7,37 +7,50 @@ let build t =
       Png.save store [Images.Save_Quality 1] (Rgba32 t)
   with Failure m -> Format.printf "Error %s@." m
 
-let _ver_strip t x y len rgb =
+let _ver_line t x y len rgb =
   let dim_x, dim_y = Images.size (Rgba32 t) in
   if x > dim_x || x < 0 || y > dim_y || y < 0 || y + len > dim_y then failwith "Out of image." else
   for y = y to y + len - 1 do
-    (* for y = 0 to dim_y -1 do *)
       Rgba32.set t x y rgb
-    (* done; *)
   done
 
-let _hor_strip t x y len rgba =
+let _hor_line t x y len rgba =
   let dim_x, dim_y = Images.size (Rgba32 t) in
   if y > dim_y || y < 0 || x > dim_x || x < 0 || x + len > dim_x then failwith "Out of image." else
-  (* if x > dim_x || x < 0 || x + len > dim_x then failwith "Out of image." else *)
   for x = x to x + len - 1 do
-    (* for x = x to dim_x -1 do *)
     Rgba32.set t x y rgba
-    (* done *)
   done
 
-(* let _tarte_linzer t x len palette =
-  let x = ref x in
-  let trou = !x in
+
+let ver_strip t x len rgb =
+  let dim_x, dim_y = Images.size (Rgba32 t) in
+  if x > dim_x || x < 0 || x + len > dim_x then failwith "Out of image." else
+  for x = x to x + len - 1 do
+    for y = 0 to dim_y -1 do
+      Rgba32.set t x y rgb
+    done;
+  done
+
+let hor_strip t y len rgb =
+  let dim_x, dim_y = Images.size (Rgba32 t) in
+  if y > dim_y || y < 0 || y + len > dim_y then failwith "Out of image." else
+  for y = y to y + len - 1 do
+    for x = 0 to dim_x -1 do
+      Rgba32.set t x y rgb
+    done
+  done
+
+let tarte_linzer t ofs bs palette =
+  let c = ref ofs in
   let i_color = ref 0 in
   try
     while true do
-      ver_strip t !x len palette.(!i_color);
-      hor_strip t !x len palette.(if !i_color + 1 < Array.length palette then !i_color + 1 else 0);
-      x := !x + trou + len;
+      ver_strip t !c bs palette.(!i_color);
+      hor_strip t !c bs palette.(if !i_color + 1 < Array.length palette then !i_color + 1 else 0);
+      c := !c + bs + ofs;
       i_color := (!i_color + 1) mod Array.length palette
     done
-  with Failure _ -> () *)
+  with Failure _ -> ()
 
 let rec build_palette size arr r g b: Color.rgba array =
   let ofs = 50 / size in
@@ -48,12 +61,12 @@ let rec build_palette size arr r g b: Color.rgba array =
   let blue  = b + (let rand = ofs + (int 50) in if b + rand > 255 then 0 else rand) in
   build_palette size (Array.append arr [|{color = {r; g; b}; alpha = 255}|]) red green blue
 
-let _build_palette size =
+let build_palette size =
   let open Random in
   let ofs = 100 / size in
   build_palette size [||] (int ofs) (int ofs) (int ofs)
 
-(* let _palette_preview t palette =
+let palette_preview t palette =
   let _dim_x, dim_y = Images.size (Rgba32 t) in
   let len = Array.length palette in
   let strip_size = dim_y / len in
@@ -61,7 +74,7 @@ let _build_palette size =
   for i = 0 to len - 1 do
     hor_strip t (i*strip_size) strip_size palette.(i)
   done;
-  hor_strip t (strip_size * len) res palette.(len-1) *)
+  hor_strip t (strip_size * len) res palette.(len-1)
 
 
 let _draw_line t xs ys xe ye =
@@ -102,7 +115,7 @@ let bresenham_n t x0 y0 x1 y1 color =
       else Rgba32.set t !curr_lr curr_sr color
   done
 
-let bresenham t x0 y0 x1 y1 color =
+let _bresenham t x0 y0 x1 y1 color =
   if x0 < x1 && y0 < y1 then bresenham t x0 y0 x1 y1 color else
   if x0 < x1 && y0 > y1 then bresenham_n t x1 y1 x0 y0 color else
   if x0 > x1 && y0 < y1 then bresenham_n t x0 y0 x1 y1 color else
@@ -112,10 +125,12 @@ let () =
   Random.self_init ();
   let rgba32 = Rgba32.create 100 100 in
   try
-    let red : Color.rgba = {color = {r = 255; g = 0; b = 0}; alpha = 255} in
-    bresenham rgba32 1 2 6 1 red;
+    palette_preview rgba32 (build_palette 5);
+    tarte_linzer rgba32 7 16 (build_palette 4);
+    (* let red : Color.rgba = {color = {r = 255; g = 0; b = 0}; alpha = 255} in *)
+    (* bresenham rgba32 1 2 6 1 red;
     bresenham rgba32 6 3 1 4 red;
     bresenham rgba32 3 5 8 7 red;
-    bresenham rgba32 8 9 4 7 red;
+    bresenham rgba32 8 9 4 7 red; *)
     build rgba32
   with Failure e -> Format.printf "ERROR: %s@." e
