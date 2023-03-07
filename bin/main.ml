8c566f5fc58637ee4c0243f368f0cf027bb6f7bb
Lucccyo <cha.git@mailo.fr>
2023-03-07 23:39:50 +0100

    ça génère des dessins d'enfants de 5 ans

--- bin/main.ml
+++ bin/main.ml
@@ -189,25 +189,37 @@ let bezier t x_list y_list color =
   let fx_list = List.map (fun x -> float_of_int x) x_list in
   let fy_list = List.map (fun y -> float_of_int y) y_list in
   let n = o - 1 in
-  let rec iter ti ox oy=
-    if ti > 1.01 then () else
-    let x = int_of_float ( bezier n 0 ti fx_list ) in
-    let y = int_of_float ( bezier n 0 ti fy_list ) in
-    if Rgba32.get t x y <> color then bresenham t ox oy x y color;
-    iter (ti +. 0.0001) x y in
-  iter 0. (List.hd x_list) (List.hd y_list)
+  let step = 0.01 in
+  let rec iter ti ox oy =
+    let x,y = if ti >= 1.0 then
+      List.hd (List.rev x_list),
+      List.hd (List.rev y_list)
+    else
+      int_of_float ( bezier n 0 ti fx_list ),
+      int_of_float ( bezier n 0 ti fy_list ) in
+    (* Format.printf "%d %d %d %d @." ox oy x y; *)
+    (*if Rgba32.get t x y <> color then*)
+    bresenham t ox oy x y color;
+    if ti < 1.0 then iter (ti +. step) x y in
+  iter step (List.hd x_list) (List.hd y_list)
 
+let rand_lst size shift max =
+  let open Random in
+  List.init size (fun _ -> shift + int max)
 
 let () =
   Random.self_init ();
   let rgba32 = Rgba32.create 300 300 in
   try
-    let red : Color.rgba = {color = {r = 255; g = 0; b = 0}; alpha = 255} in
-    let green : Color.rgba = {color = {r = 0; g = 255; b = 0}; alpha = 255} in
-    let blue : Color.rgba = {color = {r = 0; g = 0; b = 255}; alpha = 255} in
+    let open Color in
+    let palette = [|{color = {r = 255; g = 0; b = 0}; alpha = 255} ;
+                    {color = {r = 0;   g = 255; b = 0}; alpha = 255};
+                    {color = {r = 0;   g = 0; b = 255}; alpha = 255};
+                    {color = {r = 255;   g = 0; b = 255}; alpha = 255};
+                    {color = {r = 0;   g = 0; b = 0}; alpha = 255}|] in
     let white : Color.rgba = {color = {r = 255; g = 255; b = 255}; alpha = 255} in
     hor_strip rgba32 0 300 white;
-    bezier rgba32 [20; 70; 100; 120; 200; 120] [120; 20; 100; 30; 70; 100] red;
+    (* bezier rgba32 [20; 70; 100; 120; 200; 120] [120; 20; 100; 30; 70; 100] red;
     Rgba32.set rgba32 20  120 green;
     Rgba32.set rgba32 70  20  green;
     Rgba32.set rgba32 100 100 green;
@@ -220,9 +232,22 @@ let () =
     Rgba32.set rgba32 70  250 blue;
     Rgba32.set rgba32 200 150 green;
     Rgba32.set rgba32 20  150 blue;
-    Rgba32.set rgba32 150 250 blue;
+    Rgba32.set rgba32 150 250 blue; *)
+
+    (* let palette = build_palette 5 in *)
+    let r  = 42 in
+    for _ = 0 to r do
+      (* let rp = Random.int (Array.length palette) in *)
+      let rp = {color = {r = Random.int 255; g = Random.int 255; b = Random.int 255}; alpha = 255} in
+      let size = 5 in
+      let xl = (rand_lst size 4 290) in
+      let yl = (rand_lst size 4 290) in
+      bezier rgba32 xl yl rp;
+      (*for i = 0 to size - 1 do
+        Rgba32.set rgba32 (List.nth xl i) (List.nth yl i) palette.(i)
+      done;*)
+      ()
+    done;
 
     build rgba32
-
-
   with Failure e -> Format.printf "ERROR: %s@." e
