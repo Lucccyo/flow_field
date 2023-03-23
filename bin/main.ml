fc805c05bb7aa930ceb6a30f1b6bdd034acc3c4d
Lucccyo <cha.git@mailo.fr>
2023-03-23 17:04:20 +0100

    ça marche pas

--- bin/main.ml
+++ bin/main.ml
@@ -17,26 +17,10 @@ let angle_default () = Float.pi *. 0.25
 let angle_random  () = Float.pi *. (Random.float 1.)
 let angle_noise y y_max = ((float_of_int y /. float_of_int y_max)) *. Float.pi
 
-
-
-
 let vector_field dim_x dim_y space shift =
   Array.init ((dim_y / space) - (shift * 1))
     (fun y -> Array.init ((dim_x / space) - (shift * 1))
       (fun _ -> angle_noise y (dim_y / space) ))
-
-
-  (* for (column in num_columns) {
-    for (row in num_rows) {
-      (* Processing's noise() works best when the step between *)
-      (* points is approximately 0.005, so scale down to that *)
-      scaled_x = column * 0.005
-      scaled_y = row * 0.005
-      (* get our noise value, between 0.0 and 1.0 *)
-      noise_val = noise(scaled_x, scaled_y)
-      (* translate the noise value to an angle (betwen 0 and 2 * PI) *)
-      angle = map(noise_val, 0.0, 1.0, 0.0, PI * 2.0)
-      grid[column][row] = angle   } } *)
 
 let end_coord dist angle x y space =
   int_of_float (dist *. Float.cos(angle)) + (x * space),
@@ -52,7 +36,49 @@ let draw_vecs_field t =
     let x, y = x + shift, y + shift in
     let x_end, y_end = end_coord dist e x y space in
     bresenham t (x * space) (y * space) x_end y_end {color = {r = 0; g = 45; b = 200}; alpha = 255};
-  ) array_x) vecs; ()
+  ) array_x) vecs; vecs
+
+
+  (* starting point x = 500 y = 100
+
+  (* begin_curve() *)
+  for (n in [0..num_steps]) {
+      draw_vertex(x, y)
+      x_offset = x - left_x     y_offset = y - top_y
+      column_index = int(x_offset / resolution)     row_index = int(y_offset / resolution)
+      (* // NOTE: normally you want to check the bounds here *)
+      grid_angle = grid[column_index][row_index]
+      x_step = step_length * cos(grid_angle)
+      y_step = step_length * sin(grid_angle)
+      x = x + x_step     y = y + y_step } *)
+  (* end_curve() *)
+
+let curve t vecs =
+  let x = 500. in
+  let y = 500. in
+  let resolution = 20. in
+  let step_length = 0.5 in
+  let rec loop n x y =
+    if n <= 0 then ()
+    else begin
+    (* draw *)
+      try
+        single_aliased_pixel t (int_of_float x) (int_of_float y) {color ={r = 255; g = 0; b = 0}; alpha = 50};
+        (* bresenham t (int_of_float x) (int_of_float y) (int_of_float nextx) (int_of_float nexty) {color ={r = 255; g = 0; b = 0}; alpha = 50}; *)
+        Format.printf "x = %f y = %f@." x y;
+        let x_offset = x -. 20. in
+        let y_offset = x -. 20. in
+        let column_index = int_of_float (x_offset /. resolution) in
+        let row_index = int_of_float (y_offset /. resolution) in
+        (* Format.printf "%d %d@." column_index row_index; *)
+        (* Format.printf "%d %d@." (Array.length vecs) (Array.length vecs.(0)); *)
+
+        let angle = vecs.(column_index).(row_index) in
+        loop (n-1) (x +. step_length *. Float.cos(angle)) (y +. step_length *. Float.sin(angle))
+      with Invalid_argument _-> Format.printf "erro@."
+      end in
+  loop 900 x y
+
 
 let () =
   Random.self_init ();
@@ -62,6 +88,7 @@ let () =
     let white : Color.rgba = {color = {r = 255; g = 255; b = 255}; alpha = 255} in
     let red   : Color.rgba = {color = {r = 255; g = 0; b = 0}; alpha = 50} in
     hor_strip rgba32 0 image_size white;
-    draw_vecs_field rgba32;
+    let vecs = draw_vecs_field rgba32 in
+    curve rgba32 vecs;
     build rgba32
   with Failure e -> Format.printf "ERROR: %s@." e
